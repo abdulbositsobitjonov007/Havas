684950fd23c90e8c9fe6ef4371e5f0cd62a28196
abdulbositsobitjonov007 <sobitjonovabdulbosit91@gmail.com>
2025-08-02 15:33:49 +0500

    sad

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ const hammasi3 = document.getElementById("hammasi3")
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
-<<<<<<< HEAD
 
 $(".owl-carousel").owlCarousel({
     loop: true,
@@ -42,17 +41,10 @@ $(".SO-cards").owlCarousel({
     margin: 10,
     nav: true,
     dots: true,
-=======
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
->>>>>>> 41b4336436d33f35719c4a9670b149f016524ee5
     autoplay: true,
     autoplayTimeout: 3000,
     responsive: {
         0: {
-<<<<<<< HEAD
             items: 1,
         },
         600: {
@@ -84,80 +76,3 @@ $('.SO-cards').owlCarousel({
         }
     }
 })
-=======
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-})
-
-maxsulot.classList.add("maxsulot_hide")
-
-maxsulot2.classList.add("maxsulot_hide")
-
-maxsulot3.classList.add("maxsulot_hide")
-
-hammasi.classList.add("hammasi_hide")
-
-hammasi2.classList.add("hammasi_hide")
-
-hammasi3.classList.add("hammasi_hide")
-
-
-novinki.addEventListener("click", () => {
-    product.classList.add("product_hide")
-    product2.classList.add("product_hide")
-    product3.classList.add("product_hide")
-
-    hammasi.classList.add("hammasi_hide")
-    hammasi2.classList.add("hammasi_hide")
-    hammasi3.classList.add("hammasi_hide")
-})
-novinki.addEventListener("click", () => {
-    maxsulot.classList.remove("maxsulot_hide")
-    maxsulot2.classList.remove("maxsulot_hide")
-    maxsulot3.classList.remove("maxsulot_hide")
-})
-aksiya.addEventListener("click", () => {
-    maxsulot.classList.add("maxsulot_hide")
-    maxsulot2.classList.add("maxsulot_hide")
-    maxsulot3.classList.add("maxsulot_hide")
-})
-aksiya.addEventListener("click", () => {
-    product.classList.remove("product_hide")
-    product2.classList.remove("product_hide")
-    product3.classList.remove("product_hide")
-})
-vse.addEventListener("click", () => {
-    maxsulot.classList.add("maxsulot_hide")
-    maxsulot2.classList.add("maxsulot_hide")
-    maxsulot3.classList.add("maxsulot_hide")
-
-    hammasi.classList.add("hammasi_hide")
-    hammasi2.classList.add("hammasi_hide")
-    hammasi3.classList.add("hammasi_hide")
-
-    product.classList.add("product_hide")
-    product2.classList.add("product_hide")
-    product3.classList.add("product_hide")
-})
-vse.addEventListener("click", () => {
-    maxsulot.classList.add("maxsulot_hide")
-    maxsulot2.classList.add("maxsulot_hide")
-    maxsulot3.classList.add("maxsulot_hide")
-
-    product.classList.add("product_hide")
-    product2.classList.add("product_hide")
-    product3.classList.add("product_hide")
-})
-vse.addEventListener("click", () => {
-    hammasi.classList.remove("hammasi_hide")
-    hammasi2.classList.remove("hammasi_hide")
-    hammasi3.classList.remove("hammasi_hide")
-})
->>>>>>> 41b4336436d33f35719c4a9670b149f016524ee5
