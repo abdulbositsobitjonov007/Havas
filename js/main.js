fbe4bb87fa038ad81cf926cd144eee124f32f616
abdulbositsobitjonov007 <sobitjonovabdulbosit91@gmail.com>
2025-08-02 11:06:02 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -3,3 +3,64 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        1400: {
+            items: 1,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+        },
+    },
+});
+
+$(".SO-cards").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
+});
+
+$('.SO-cards').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
